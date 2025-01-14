5a5cf52ea294ebe7fae8f61d88a16f23b98e00f3
Abdul Gaffar <gaffarking786@gmail.com>
2025-01-14 14:54:53 +0530

    Updates

---
+++ precache-manifest.a0d69dff63d90536cd4f9348807d3ebc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6ff4f0742e2bb4074f114bc1d5d0d58",
+    "url": "/mercyfoundation/index.html"
+  },
+  {
+    "revision": "345f67edf225a6c6bd5e",
+    "url": "/mercyfoundation/static/css/main.e92f10dc.chunk.css"
+  },
+  {
+    "revision": "7124542ed7c908ea5bff",
+    "url": "/mercyfoundation/static/js/2.e06e3f26.chunk.js"
+  },
+  {
+    "revision": "d207fd5069793021567ce6aa429b79a9",
+    "url": "/mercyfoundation/static/js/2.e06e3f26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edb591d20bce22961d02",
+    "url": "/mercyfoundation/static/js/3.a9d4af82.chunk.js"
+  },
+  {
+    "revision": "345f67edf225a6c6bd5e",
+    "url": "/mercyfoundation/static/js/main.f38b8d17.chunk.js"
+  },
+  {
+    "revision": "9e6f4403a0e730d1f2c9",
+    "url": "/mercyfoundation/static/js/runtime-main.2c727ea0.js"
+  }
+]);
